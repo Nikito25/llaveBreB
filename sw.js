2cf87cde872f35d2acb77c002970730f69b892fe
Nikito25 <santiago.j.bono@gmail.com>
2026-06-22 23:29:30 -0300

    Camara: alta resolucion + autofoco continuo + zoom + escaneo de zona central para QR chicos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - cachea la app para uso offline tras la 1a visita */
-const CACHE = 'breb-v3';
+const CACHE = 'breb-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
