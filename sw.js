7fbb687a44847a85b0e8172b161df88f7aedbb05
Nikito25 <santiago.j.bono@gmail.com>
2026-07-11 18:46:48 -0300

    Zoom automatico ~2x al abrir camara + captura mas rapida (sin takePhoto lento)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - cachea la app para uso offline tras la 1a visita */
-const CACHE = 'breb-v10';
+const CACHE = 'breb-v11';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
